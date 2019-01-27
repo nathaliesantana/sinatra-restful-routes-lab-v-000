59eeb9b2b0e4adfcb8791b320041ca4980d4ebc1
Nathalie Santana <nathalie.scruz@gmail.com>
2019-01-27 05:15:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,37 @@
+
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    binding.pry
+    erb :index
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to '/recipes/#{@recipe.id}'
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do #edit form
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :edit
+end
+
+
+
 
 end
